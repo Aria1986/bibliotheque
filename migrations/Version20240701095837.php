f7c095a2bf6850d6c4d075db155486491662e555
maimai <mailys.andrieu.simplon@gmail.com>
2024-07-02 16:42:35 +0200

    connection ok

--- migrations/Version20240701095837.php
+++ migrations/Version20240701095837.php
@@ -22,7 +22,6 @@ final class Version20240701095837 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE author (id INT AUTO_INCREMENT NOT NULL, author_name VARCHAR(255) NOT NULL, biography VARCHAR(500) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, title VARCHAR(255) NOT NULL, INDEX IDX_CBE5A331F675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A331F675F31B FOREIGN KEY (author_id) REFERENCES author (id)');
         $this->addSql('ALTER TABLE category_book ADD CONSTRAINT FK_407ED97616A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE');
@@ -35,7 +34,6 @@ final class Version20240701095837 extends AbstractMigration
         $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A331F675F31B');
         $this->addSql('DROP TABLE author');
         $this->addSql('DROP TABLE book');
-        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,61 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-    function new(Request $request): Response
-    {
-        // creates a task object and initializes some data for this example
-        $task = new User();
-        $task->setTask('Write a blog post');
-        $task->setDueDate(new \DateTimeImmutable('tomorrow'));
-
-        $form = $this->createFormBuilder($task)
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, ['label' => 'Create Task'])
-            ->getForm();
-
-        // ...
-    }
-
-    #[Route('/inscription', name:"app_inscription")]
-    public function formInscription(): Response
-    {
-        return $this ->render('user/inscription.html.twig');
-    }
-
-    #[Route('/contact', name:'app_contact')]
-    public function contactForm():Response
-    {
-        return $this->render('contact.html.twig');
-    }
-    // #[Route('/createUser', name: 'app_create_user')]
-    // public function createUser(): 
-    // {
-    //     $stmt = $pdo->prepare("UPDATE users SET name = :name WHERE email = :email");
-    //     $name = 'Alice Johnson';
-    //     $email = 'alice@example.com';
-    //     $stmt->bindParam(':name', $name);
-    //     $stmt->bindParam(':email', $email);
-    //     $stmt->execute();
-
-    //     echo "User name updated successfully";
-            
-    // }
-
-
+    
 }
